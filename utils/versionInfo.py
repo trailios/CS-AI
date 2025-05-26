0c3cde3d200dec01363b0b0383d2ae18625b9815
syntax3d <doctor82@gmail.com>
2025-05-26 19:19:39 +0530

    ee

--- utils/versionInfo.py
+++ utils/versionInfo.py
@@ -1,9 +1,25 @@
 from curl_cffi import requests
 
+headers = {
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'no-cache',
+    'pragma': 'no-cache',
+    'referer': 'https://www.roblox.com/',
+    'sec-ch-ua': '"Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'script',
+    'sec-fetch-mode': 'no-cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
+    # 'cookie': 'RBXEventTrackerV2=CreateDate=05/25/2025 08:01:11&rbxid=&browserid=1748178071985001; GuestData=UserID=-2088128126; RBXSource=rbx_acquisition_time=05/25/2025 13:01:17&rbx_acquisition_referrer=https://www.roblox.com/login&rbx_medium=Social&rbx_source=www.roblox.com&rbx_campaign=&rbx_adgroup=&rbx_keyword=&rbx_matchtype=&rbx_send_info=0; _cfuvid=2Tcy_EqLJ0qhYnviOGCTm_1P9sZBMu9.hAx5FSqz2z4-1717104076479-0.0.1.1-604800000; rbx-ip2=1',
+}
 def get_version_info(apiurl,sitekey):
-        captchadata=requests.get(f"{apiurl}/v2/{sitekey}/api.js",headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7','accept-language': f'en-US,en;q=0.9,{locale};q=0.8,{locale.split("-")[0]};q=0.7','cache-control': 'max-age=0','device-memory': '8','priority': 'u=0, i','sec-ch-dpr': '1','sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"','sec-ch-ua-arch': '"x86"','sec-ch-ua-bitness': '"64"','sec-ch-ua-form-factors': '"Desktop"','sec-ch-ua-full-version-list': '"Google Chrome";v="129.0.6668.60", "Not=A?Brand";v="8.0.0.0", "Chromium";v="129.0.6668.60"','sec-ch-ua-mobile': '?0','sec-ch-ua-model': '""','sec-ch-ua-platform': '"Windows"','sec-ch-ua-platform-version': '"10.0.0"','sec-ch-viewport-width': '1133','sec-fetch-dest': 'document','sec-fetch-mode': 'navigate','sec-fetch-site': 'none','sec-fetch-user': '?1','upgrade-insecure-requests': '1','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',}
-        ).text.split("/enforcement.")
+        captchadata=requests.get(f"{apiurl}/v2/{sitekey}/api.js",headers=headers
+        ).text.split('/enforcement.')
 
         capi_version=captchadata[0].split('"')[-1]
         enforcement_hash=captchadata[1].split('.html')[0]
         return [capi_version, enforcement_hash]
+        
