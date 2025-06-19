74d02ea5b681b805ae8f7ee5532a9546496c5e0b
trailios <noonewilllikeme@gmail.com>
2025-06-19 17:33:22 +0200

    type shit

--- src/utils/ipIntelligence.py
+++ src/utils/ipIntelligence.py
@@ -1,9 +1,43 @@
 import requests
-import pytz
-
-from datetime import datetime
 
 from src.helpers.ProxyHelper import Proxy
+
+from typing                 import Dict, Optional
+
+class TimeZoneOffsets:
+    def __init__(self) -> None:
+        self._timezones: Dict[str, int] = {
+            "Europe/London": 0,
+            "Europe/Berlin": 60,
+            "Europe/Moscow": 180,
+            "Africa/Lagos": 60,
+            "Africa/Cairo": 120,
+            "Africa/Nairobi": 180,
+            "Asia/Dubai": 240,
+            "Asia/Karachi": 300,
+            "Asia/Kolkata": 330,
+            "Asia/Dhaka": 360,
+            "Asia/Bangkok": 420,
+            "Asia/Shanghai": 480,
+            "Asia/Tokyo": 540,
+            "Australia/Sydney": 600,
+            "Pacific/Auckland": 720,
+            "Pacific/Honolulu": -600,
+            "America/Anchorage": -540,
+            "America/Los_Angeles": -480,
+            "America/Denver": -420,
+            "America/Chicago": -360,
+            "America/New_York": -300,
+            "America/Caracas": -240,
+            "America/Sao_Paulo": -180,
+            "Atlantic/Azores": -60,
+        }
+
+    def get_offset(self, timezone: str) -> Optional[int]:
+        return self._timezones.get(timezone, -120)
+
+    def list_timezones(self) -> list[str]:
+        return list(self._timezones.keys())
 
 
 def getIpInfo(proxy: Proxy) -> int:
@@ -19,9 +53,8 @@ def getIpInfo(proxy: Proxy) -> int:
 
         timezone_str = geo_data.get("timezone", "America/New_York")
 
-        tz = pytz.timezone(timezone_str)
-        current_time = datetime.now(tz)
-        utc_offset = int(current_time.utcoffset().total_seconds() / 60)
+        timetones = TimeZoneOffsets()
+        utc_offset = timetones.get_offset(timezone_str) or 0
 
         return utc_offset
     except Exception:
