bc2c073f802a70eb873581a9b6431508a4d92b9a
trailios <noonewilllikeme@gmail.com>
2025-07-09 17:54:05 +0200

    updzs

--- src/__init__.py
+++ src/__init__.py
@@ -15,14 +15,15 @@ internal_session.headers = {
     "cache-control": "no-cache",
     "pragma": "no-cache",
     "referer": "https://www.roblox.com/",
-    "sec-ch-ua": '"Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"',
+    "sec-ch-ua": '"Chromium";v="138", "Google Chrome";v="136", "Not.A/Brand";v="99"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": '"Windows"',
     "sec-fetch-dest": "script",
     "sec-fetch-mode": "no-cors",
     "sec-fetch-site": "same-site",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
 }
 
-log = ...
+log = ... # new logger aint impleented yet
 tools: Utils = Utils
+
