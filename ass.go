16c58c0cbec685de1478c8340352b818aff1006e
trailios <noonewilllikemr@gmail.com>
2025-08-11 14:26:44 -0700

    as

--- ass.go
+++ ass.go
@@ -73,7 +73,6 @@ func startProcess(bda string, proxy string, blob string, surl string, pkey strin
 		{"bda", bda},
 		{"site", "https://www.roblox.com"},
 		{"userbrowser", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"},
-		{"userbrowser", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"},
 		{"data[blob]", string(blob)},
 	}
 	var parts []string
